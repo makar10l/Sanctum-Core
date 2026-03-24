34d854cd42f801b336916e6fa8f9825ec0f7f758
makar10l <makar10l.mail@proton.me>
2026-03-24 22:09:09 +0000

    updated : I2C driver to 0_1v(test version and fuck it i wrote this asshole at 1PM)

--- lib/i2c_driver/i2c.c
+++ lib/i2c_driver/i2c.c
@@ -3,29 +3,51 @@
 #include <PinInitializer.h>
 #include <uart.h>
 #include <PWM_driver.h>
-UARTPackage _msg = {
-    .chars = "ACK",
-    .size = 3,
-};
+#define OLED_ADDRESS 0x3C
+uint8_t i2c_device_address;
+uint16_t dataidx = 0;
+uint8_t* dataptr;
+uint8_t data_size = 0;
 uint8_t I2C_init(uint16_t clk_freq_hz){
+    NVIC_EnableIRQ(I2C2_EV_IRQn);
     RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
     PinInit(GPIOB, 10, 0b1111);
     PinInit(GPIOB, 11, 0b1111);
     I2C2->CR2 = APB1_FREQ_MHZ;
+    I2C2->CR2 |= I2C_CR2_ITEVTEN | I2C_CR2_ITBUFEN;
     I2C2->CCR = (APB1_FREQ_MHZ * 1000000) / (2 * clk_freq_hz);
-    I2C2->TRISE = (APB1_FREQ_MHZ * 1000 / 1000) + 1;
+    I2C2->TRISE = (APB1_FREQ_MHZ * 1000 / 1000) + 1;   
     I2C2->CR1 |= I2C_CR1_PE;
     return 0;
 }
-//rewrite
-// void I2C_SentAddress(uint8_t addr_7bit)
-// {
-//     //while(I2C2->SR2 & I2C_SR2_BUSY);           // ждем шину
-//     I2C2->CR1 |= I2C_CR1_START;                // START
-//     //while(!(I2C2->SR1 & I2C_SR1_SB));          // ждем START
-//     I2C2->DR = (addr_7bit << 1) | 0;           // адрес + запись
-//     //while(!(I2C2->SR1 & I2C_SR1_ADDR));       // ждем ACK
-//     UartSend(&_msg);
-//     SetLampBrightness(25);
-//     (void)I2C2->SR1; (void)I2C2->SR2;          // сброс ADDR
-// }
+
+int8_t I2C_sent(uint8_t addr, UARTPackage data){
+    if(I2C2->SR2 & I2C_SR2_BUSY){
+        return -1;
+    }
+    data_size = data.size;
+    dataptr = (uint8_t*)data.chars;
+    i2c_device_address = addr;
+    dataidx = 0;
+    I2C2->CR1 |= I2C_CR1_START;  
+    return 0;  
+}
+
+void I2C2_EV_IRQHandler(){
+    if(I2C2->SR1 & I2C_SR1_SB){
+        I2C2->DR = (i2c_device_address << 1) | 0; 
+    }
+    if(I2C2->SR1 & I2C_SR1_ADDR){
+        (void)I2C2->SR2; 
+        I2C2->DR = dataptr[dataidx];
+        dataidx++;
+    }
+    if(I2C2->SR1 & I2C_SR1_TXE){
+        if(dataidx >= data_size){
+            I2C2->CR1 |= I2C_CR1_STOP;
+            return;
+        }
+        I2C2->DR = dataptr[dataidx];
+        dataidx++;
+    }
+}
--- lib/i2c_driver/i2c.h
+++ lib/i2c_driver/i2c.h
@@ -1,10 +1,7 @@
 #ifndef I2C
 #define I2C
 #include <stm32f1xx.h>
-typedef struct{
-    char* chars;
-    int size;
-} I2C_package;
+#include <uart.h>
     uint8_t I2C_init(uint16_t clk_freq_hz); 
-    uint8_t I2C_send(uint16_t address, I2C_package pkg);
+    int8_t I2C_send(uint16_t address, UARTPackage data);
 #endif
